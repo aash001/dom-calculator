3934d11231f254d56250183d0b7f0326247411ab
aampinar <98187277+aampinar@users.noreply.github.com>
2022-04-20 12:20:47 -0400

    Add function to switch operators for multiplication and division

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const screenDisplay = document.querySelector("#screen");
     const spans = document.querySelectorAll('.buttons span');
     const spanArray = Array.from(spans);
-    const division = document.querySelectorAll('.operator')[1]; //.innerText = '/';
-    const multiplication = document.querySelectorAll('.operator')[2]; //.innerText = '*';
+
+    function operatorSwitch(symbol) {
+        let newOperator = symbol
+        if (symbol.includes("÷")) {
+            newOperator = symbol.replace('÷', '/')
+        } else if (symbol.includes("x")) {
+            newOperator = symbol.replace('x', '*')
+        }
+        return newOperator
+    }
 
     spanArray.map(button => button.addEventListener('click', function () {
         if (button.innerText !== "=") {
             screenDisplay.innerText += button.innerText
         };
-        if (button.innerText === "÷") {
-            screenDisplay.innerText = eval(division.replace('÷', '/')) //write a function for replace
-        };
         if (button.innerText === "=") {
-            screenDisplay.innerText = eval(screenDisplay.innerText)
-            console.log(screenDisplay)
+            screenDisplay.innerText = eval(operatorSwitch(screenDisplay.innerText))
         };
         if (button.innerText === "C") {
             screenDisplay.innerText = ""
